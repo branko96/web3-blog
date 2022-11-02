58c298caa05f4b9873fdd5d8b4aaba08438a8a1f
branko96 <branko-96@hotmail.com>
2022-11-02 17:25:51 -0300

    Fixes

--- components/my-component/Video.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export type MyComponentProps = {}
-
-export default function Video({}: MyComponentProps) {
-    return <video src="https://www.loom.com/share/99204a7842bd4b608bbbd812f61d3cf7"></video>;
-}
